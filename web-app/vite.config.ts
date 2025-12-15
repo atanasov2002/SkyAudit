6e2e1aef479c7eef98f389d4523e36cb6769b869
Atanas Atanasov <atanasvoatanas2002@gmail.com>
2025-12-15 18:38:20 +0100

    Fix vite.config.ts for deploymen on vercel

--- web-app/vite.config.ts
+++ web-app/vite.config.ts
@@ -3,9 +3,10 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { reactRouterDevTools } from "react-router-devtools";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouterDevTools(), reactRouter(), tsconfigPaths(), ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -20,4 +21,7 @@ export default defineConfig({
   ssr: {
     noExternal: ["styled-components"],
   },
+  build: {
+    outDir: 'dist'
+  }
 });

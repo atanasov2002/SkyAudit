62e6a5cd153ecbdd05f836ea6728973013ba7ef9
Atanas Atanasov <atanasvoatanas2002@gmail.com>
2025-12-15 18:46:15 +0100

    Fix vite.config.ts for deploymen on vercel

--- web-app/vite.config.ts
+++ web-app/vite.config.ts
@@ -3,35 +3,24 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { reactRouterDevTools } from "react-router-devtools";
 
-export default defineConfig(async ({ command }) => {
-  const plugins = [tailwindcss(), reactRouter(), tsconfigPaths()];
-
-  if (command === 'serve') {
-    // Only import devtools in dev mode
-    const { reactRouterDevTools } = await import('react-router-devtools')
-    plugins.push(reactRouterDevTools())
+export default defineConfig({
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      components: path.resolve(__dirname, "./components"),
+    },
+  },
+  server: {
+    proxy: {
+      "/api": "http://localhost:4001",
+    },
+  },
+  ssr: {
+    noExternal: ["styled-components"],
+  },
+  build: {
+    outDir: 'dist'
   }
-  
-  return {
-    
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-        components: path.resolve(__dirname, "./components"),
-      },
-    },
-    server: {
-      proxy: {
-        "/api": "http://localhost:4001",
-      },
-    },
-    ssr: {
-      noExternal: ["styled-components"],
-    },
-    build: {
-      outDir: 'dist'
-    }
-  };
 });
